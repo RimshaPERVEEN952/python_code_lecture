c1945a686a472dfb4a315df96fb2f2ff47eb832d
Rimsha Perveen <saleemabid832@gmail.com>
2025-01-11 14:28:36 +0500

    Add files via upload

---
+++ python_project_table_.py
@@ -0,0 +1,33 @@
+#range
+# tables
+integer = int(input("enter yoyr number:\t"))
+for i in range(  1 , 11 ):
+    print("\t" , integer , "* " , i , " =", i*integer)
+
+
+
+
+
+🖥️ Just Built a Multiplication Table in Python! ✨
+
+
+
+I’ve been working on a small project where I wrote Python code to generate a multiplication table. It was a great exercise in loops and user input, and it feels amazing to see the code come to life! 🔢
+
+
+
+🔹 Key takeaway: Understanding how loops can simplify repetitive tasks in coding.  
+
+
+
+Excited to keep learning and building more projects like this! 🚀
+
+
+
+Google colab Link:
+
+https://colab.research.google.com/drive/1J5qoyz_4HC3sc71csCDXSZK5okxQIbnf#scrollTo=hw7wqYD1w3-I
+
+
+
+#Python #CodingJourney #LearningToCode #TechProjects #Programming #PythonProjects
