e14e8169641c1510d2df1c081d7d85f7c9b8acf9
Rimsha Perveen <saleemabid832@gmail.com>
2025-01-26 02:42:12 +0500

    Add files via upload

---
+++ web.py
@@ -0,0 +1,156 @@
+
+import streamlit as web
+
+web.set_page_config(page_title="Landing_page_in_python" , layout="centered")
+
+web.title("$$$ WelCome to$$$")
+web.title("$$$ 📑 THE  CITIZENS FOUNDATION $$$") 
+# web.camera_input("") 
+web.title("The Citizens Foundtion")
+web.image("https://www.tcf.org.pk/wp-content/uploads/2022/11/Revamping-Computer-Labs-wecompress.com_-1024x683.jpg" , use_container_width=True)
+web.write( """
+At The Citizen Foundation (TCF), we are committed to creating a brighter future for the children of Pakistan through quality education. Founded in 1995, TCF has grown to become one of the largest networks of schools in Pakistan, empowering thousands of children with the skills and knowledge they need to succeed in life.""")
+web.write("---")
+
+# web.header("rimshaparveen952@gmail.com")
+web.title("Our Mission")
+web.write("""
+Our mission is to provide quality education to underprivileged children in Pakistan, giving them the tools to transform their lives and contribute to the prosperity of their communities. At TCF, we believe that education is a fundamental right and the key to breaking the cycle of poverty.""")
+web.write("---")
+
+web.title("What We Do....")
+web.header("1. Schools and Education Programs: ")
+web.write("""TCF operates a network of over 1,600 school units across Pakistan, focusing on providing free education to children in marginalized communities. Our schools offer a well-rounded curriculum, including mathematics, science, social studies, and arts, ensuring that every child receives the education they deserve.""")
+
+web.header("2. Empowering Communitie:")
+web.write("""
+\tWe work closely with local communities to ensure that our schools are integrated into the fabric of each area. Our aim is not only to provide education but also to encourage community involvement and create sustainable change.
+""")
+
+web.header("3. Teacher Training and Development:")
+web.write("""
+\tWe are deeply committed to ensuring that our teachers are well-trained, motivated, and equipped to deliver high-quality education. Our professional development programs focus on improving teaching techniques and pedagogical skills, empowering teachers to make a real difference in the lives of their students.
+""")
+web.header("4. Scholarships and Financial Assistance ")
+web.write("""
+\tWe provide scholarships and financial assistance to students from low-income families to ensure that financial constraints do not limit their educational opportunities. TCF is dedicated to ensuring that all children, regardless of their background, have access to the best educational resources.""")
+
+web.write("---")
+web.title("""Our Impact\n
+Since its inception, The Citizen Foundation has made a significant impact on the lives of thousands of children. Here are some key milestones:""")
+web.image("https://www.tcf.org.pk/wp-content/uploads/2022/11/Blended-Learning-1024x683.jpeg" , use_container_width=True)
+web.header(".Over 1.6 Million Students Impacted:")
+web.write("""\tWe have educated more than 1.6 million children, helping them secure a better future.""")
+
+web.header(".1,600+ School Units:")
+web.image("https://scontent.fkhi16-2.fna.fbcdn.net/v/t39.30808-6/369259809_694415382724934_7443330445050658306_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=O10NoKhzOCMQ7kNvgEwEvro&_nc_oc=Adi1H4oGjqefn6yE7MQZEHggC51aCMZuwq2SNxqBn54olGp44Nhn45OunVN0QD1_g1g&_nc_zt=23&_nc_ht=scontent.fkhi16-2.fna&_nc_gid=AL-NtlMn8WYLstcFtwRTsIv&oh=00_AYDT3-7Ye6ukwn-xI1sqDVR2yG7MKPXsPVKCImOpud4kqg&oe=679B0AB7" , use_container_width=True)
+web.write("""\tOur schools are spread across urban and rural areas in Pakistan, providing education to children who otherwise would not have had access.
+""")
+web.image("https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/474263988_1042854994547636_2809968128268191841_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=OpYdTRL_YzoQ7kNvgGhKiMF&_nc_oc=AdjNO0pFrKGfTlend2g1y-6Sio3HZ5SGkngRfKC5l2GsVIGOw_AYt2O-qMO8P_Ue6S8&_nc_zt=23&_nc_ht=scontent.fkhi16-1.fna&_nc_gid=AN9pHCQ2gvEzbamxnTfsaKV&oh=00_AYDzhgviEgC7MbSttnmGfnU3RFZCZGKhBToa3RBKeRQS0A&oe=679AF46F" , use_container_width=True)
+web.image("https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/473647686_1039062784926857_596641914932691839_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=BMHQ8LLLKqgQ7kNvgEIRHEM&_nc_oc=Adik-L2376R_0kzXlKlSFxQPOe4XZKddaSZfIGHuPaxb-pqON_dJijNS-ibG0mxynic&_nc_zt=23&_nc_ht=scontent.fkhi16-1.fna&_nc_gid=AN9pHCQ2gvEzbamxnTfsaKV&oh=00_AYCD5ZSYzIgTGHQPK1hA6_wfXd0i0WaxjcF2FBnrkzd1rQ&oe=679B2891" , use_container_width=True)
+web.header(".High Graduation Rate")
+web.write("""\thave consistently performed well in their exams, with many going on to pursue higher education and successful careers.""")
+web.write("\nAt TCF, we believe in nurturing future leaders who will contribute to the development of Pakistan. Our alumni are a testament to the success of our programs, having excelled in various fields like medicine, engineering, business, and public service.")
+web.write("---")
+
+web.title("Our Core Values")
+web.header(".Integrity:")
+web.video("https://youtu.be/dtD84ixt4ec?si=XcYaM4vGOmllww7B")
+web.write("\t We believe in honesty, transparency, and ethical practices in all our operations.")
+
+web.header(".Commitment:")
+web.video("https://youtu.be/JUxdh2ynipM?si=xSCtd1uDxWT4j805")
+web.write("\t We are dedicated to our mission of providing quality education and ensuring that every child has the opportunity to succeed.")
+
+web.header(".Empathy:")
+web.write("\tWe work with compassion to understand and address the needs of children, their families, and communities.")
+
+web.header(".Innovation:")
+web.write("\t We constantly strive to improve and innovate in our teaching methodologies and school infrastructure to offer the best education.")
+
+web.title("$$$our students $$$")
+web.image("https://scontent.fkhi16-2.fna.fbcdn.net/v/t39.30808-6/274995276_10226593845935192_77632114588304005_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=r12zOsFGw0AQ7kNvgGcKUyA&_nc_oc=Adj2LNTAO2dAmijpGThGf4_EZtsfTsIxgtC_z5liqroxPcCXFtdQq4Xwd19FIRnO0ss&_nc_zt=23&_nc_ht=scontent.fkhi16-2.fna&_nc_gid=ACYAlakW83JNmGuRX1w0DgR&oh=00_AYDNlbUwWcK3Z129FsfKUtt1PUwlNRQcbqeWAVIqHN-u5w&oe=679B1DCF" , use_container_width=True)
+web.title("How You Can Help")
+web.write("The Citizen Foundation believes in the power of community support. Whether you’re a student, a professional, or an organization, there are several ways you can contribute to our cause:")
+
+web.header(".Donate: ")
+web.image("https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/472802658_1035456655287470_7015860860756899871_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=I8F-gkPrAx4Q7kNvgHLrH6G&_nc_oc=AdiKR7yCKlxpWgiXHIiWrP8PrTPDMbDdLRGKR8FOCrGsbR0KULHu-vS5TCaIfiztt2E&_nc_zt=23&_nc_ht=scontent.fkhi16-1.fna&_nc_gid=Ah774X2MYICauio4bB__b4_&oh=00_AYCAmA4KLVPrQdDoTpJVS5jK7AV0MRnGKnM0FYC2EpvfLQ&oe=679B0D13" , use_container_width=True)
+web.image("https://tcf-wp.tcfglobal.org/wp-content/uploads/2024/04/09223946/Pak.jpg", use_container_width=True)
+web.write(" Your financial contributions go directly to funding our educational programs, helping us expand our reach and support more children.")
+
+web.header(".Volunteer:")
+web.write("Get involved by volunteering your time and skills. Whether you’re mentoring students, assisting in administrative tasks, or sharing your expertise, every bit of help counts.")
+web.video("https://www.facebook.com/share/p/18UcDm1Cgp/")
+web.video("https://youtu.be/rbQks44YG9s?si=h7tHOMMBW-2ygdVs")
+web.header(".Fundraise: ")
+web.write(" Join our fundraising initiatives to raise awareness and funds for TCF. Organize events, drives, or campaigns to support the cause of education.")
+
+web.header(".Corporate Partnerships:")
+web.write("- Partner with us to create a lasting social impact through educational sponsorships, donations, or collaborative initiatives.")
+web.write("---")
+
+web.title("Join the Movement")
+web.image("https://www.tcf.org.pk/wp-content/uploads/2019/04/Movement2.jpg" , use_container_width=True)
+web.subheader("""
+Education is not just about teaching children; it’s about empowering entire communities to build a better tomorrow. By supporting The Citizen Foundation, you become a part of this transformative journey. Together, we can break the cycle of poverty, empower future generations, and create a more equitable society.
+
+Visit our [Contact Us](#) page for more information on how to get involved, or reach out to us via phone or email.
+
+Let’s work together to give every child in Pakistan the education they deserve. Your support makes all the difference.
+
+---""")
+
+web.title("Our Partners")
+web.write("""We are proud to be supported by numerous local and international partners, whose contributions help us provide quality education across Pakistan. Together, we can continue to make a lasting impact on the future of our children.
+""")
+web.write("---")
+
+web.title("Contact Us")
+web.write("""
+For more information or to get involved, please feel free to reach out to us:""")
+
+web.header(""".Email:[info@tcf.org](info@tcf.org)""")
+
+
+web.header(""".Phone:+92(21)35689999""")
+
+
+web.header(""".Address:Plot No. 20, Sector - 14, Near Brookes Chowrangi, Korangi Industrial Area,
+Karachi 74900, Pakistan. SECP Registration Number: 0037144.""")
+web.write("---")
+web.title("$$$Stay Connected$$$")
+web.write("Follow us on social media to stay updated on our latest projects, events, and success stories:")
+web.header(".Facebook:[(https://www.tcf.org.pk)]((https://www.tcf.org.pk))")
+
+web.header(""".Twitter:[@TCF_Official](@TCF_Official)""")
+web.header(".Instagram [@thecitizenfoundation](@thecitizenfoundation)")
+
+web.header(".LinkedIn:The Citizen Foundation")
+web.write("---")
+web.write("---")
+web.title("""
+The Citizen Foundation - Educating Pakistan’s Future""")
+web.write("---")
+
+web.subheader("$$$DO YOU WANT TO ENROLL,$$$ ")
+web.write("let's get started")
+name = web.text_input("Enter Your Name :\t")
+f_name =web.text_input("Enter Your Father Name:\t")
+qualification = web.text_input("Enter Your Qualification:\t")
+age = web.text_input("Enter Your age:\t")
+email = web.text_input("Enter Your email address:\t")
+
+button = web.button("submit")
+web.subheader("Rimsha Parveen")
+web.title("myFirst website in python")
+if button:
+    
+    web.markdown(f"""
+new Candidate\n
+                 \tname: \t{name}
+                 father name:\t{f_name}
+                 qualification:\t{qualification}
+                 age:\t{age}
+                 email address:\t{email}""")
+
+                 
